debffa8078d584ce9a3147caea153a38ff7fbeae
ZAAALJLJ <152788193+ZAAALJLJ@users.noreply.github.com>
2025-04-23 22:01:05 +0800

    Create + api added

--- backend/routes/route.py
+++ backend/routes/route.py
@@ -7,19 +7,24 @@ from bson import ObjectId
 router = APIRouter()
 
 # GET Request all cards
-@router.get("/")
+@router.get("/flashcards")
 async def get_cards():
     flashcard = list_flashcard(flashcard_collection.find())
     return flashcard
 
-@router.post("/")
+@router.get("/flashcard/{id}")
+async def get_card():
+    flashcard = flashcard_collection.find_one({"_id": ObjectId(id)})
+    return flashcard
+
+@router.post("/flashcards")
 async def create_cards(cards: Flashcard):
     flashcard_collection.insert_one(dict(cards))
     
-@router.put("/{id}")
+@router.put("/flashcards/{id}")
 async def update_card(id: str, card: Flashcard):
     flashcard_collection.find_one_and_update({"_id": ObjectId(id)}, {"$set": dict(card)})
     
-@router.delete("/{id}")
+@router.delete("/flashcards/{id}")
 async def delete_card(id: str):
     flashcard_collection.find_one_and_delete({"_id": ObjectId(id)})
--- frontend/src/pages/CreateFlashcard.jsx
+++ frontend/src/pages/CreateFlashcard.jsx
@@ -1,6 +1,44 @@
+import { useEffect, useState } from 'react';
 import '../css/CreateFlashcard.css'
+import api from '../api'
 
 function CreateFlashcard () {
+    const [flashcards, setCards] = useState([]);
+    const [flashcard, setCard] = useState({question: '', answer: ''});
+
+    // GET all cards
+    const fetchCards = async () => {
+        try {
+            const response = await api.get('/flashcards/');
+            console.log('Fetched cards:', response.data);
+            setCards(response.data);
+        } catch(error) {
+            console.error('Error fetching cards:', error);
+        }
+    };
+
+    // SET cards
+    useEffect(() => {
+        fetchCards();
+    }, []);
+
+    // SET single card
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setCard({ ...flashcard, [name]: value});
+    }
+
+    // ADD cards to db
+    const handleAddCard = async () => {
+        try {
+            await api.post('/flashcards/', flashcard);
+            setCards([...flashcards, flashcard]);
+            setCard({ question: '', answer: ''});
+        } catch (error) {
+            console.error('Error adding cards:', error);
+        }
+    };
+
     return (
         <div className="create-page">
             <div className="create-nav-bar">
@@ -10,24 +48,41 @@ function CreateFlashcard () {
             </div>
             <div className="create-content">
                 <div className="create-flashcard-content">
-                    <div className='create-qa-container'>
+                    <form className='create-qa-container' onSubmit={handleAddCard}>
                         <div className='create-question'>
-                            <div className='question-title'>
+                            <label htmlFor='question' className='question-title'>
                                 Question
-                            </div>
-                            <input className='question' type='text' placeholder='Type question here'/>
+                            </label>
+                            <input 
+                                className='question' 
+                                type='text' 
+                                id='question'
+                                name='question'
+                                placeholder='Type question here'
+                                value={flashcard.question}
+                                onChange={handleInputChange}
+                            />
                         </div>
                         <div className='create-answer'>
-                            <div className='answer-title'>
+                            <label htmlFor='answer' className='answer-title'>
                                 Answer
-                            </div>
-                            <input className='answer' type='text' placeholder='Type answer here'/>
+                            </label>
+                            <input 
+                                className='answer' 
+                                type='text' 
+                                id='answer'
+                                name='answer'
+                                placeholder='Type answer here'
+                                value={flashcard.answer}
+                                onChange={handleInputChange}
+                            />
                         </div>
                         <div className='save-flashcard-btn'>
-                            <button className='btn-save'>Save</button>
-                            <button className='btn-save'>Delete</button>
+                            <button type='button' className='btn-save' onClick={handleAddCard}>Save</button>
+                            <button type='button' className='btn-save'>Delete</button>
                         </div>
-                    </div>
+                    </form>                
+
                 </div>
                 <div className="created-flashcards">
                     <button className="btn-create">
