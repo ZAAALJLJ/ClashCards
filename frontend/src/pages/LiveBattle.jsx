9f2653381d220920cc892b1f4639b95f3ce202f8
Sloth-iNkt <ciavelm18@gmail.com>
2025-04-30 20:31:53 +0800

    fix: live battle input items alignment

--- frontend/src/pages/LiveBattle.jsx
+++ frontend/src/pages/LiveBattle.jsx
@@ -345,9 +345,11 @@ const handleLeaveBattle = () => {
                                         aria-label={isTimeUp || isQuizFinished ? "Quiz Over" : "Send answer"}
                                     >
 
-                                        <svg viewBox="0 -0.5 21 21" xmlns="http://www.w3.org/2000/svg" className="paper-plane-icon-sm">
-                                            <path d="M2.61258 9L0.05132 1.31623C-0.22718 0.48074 0.63218 -0.28074 1.42809 0.09626L20.4281 9.0963C21.1906 9.4575 21.1906 10.5425 20.4281 10.9037L1.42809 19.9037C0.63218 20.2807 -0.22718 19.5193 0.05132 18.6838L2.61258 11H8.9873C9.5396 11 9.9873 10.5523 9.9873 10C9.9873 9.4477 9.5396 9 8.9873 9H2.61258z"  fill={isTimeUp ? "#cccccc" : "currentColor"}/>
-                                        </svg> 
+                                        {!(isTimeUp || isQuizFinished) && (
+                                            <svg viewBox="0 -0.5 21 21" xmlns="http://www.w3.org/2000/svg" className="paper-plane-icon-sm">
+                                            <path d="M2.61258 9L0.05132 1.31623C-0.22718 0.48074 0.63218 -0.28074 1.42809 0.09626L20.4281 9.0963C21.1906 9.4575 21.1906 10.5425 20.4281 10.9037L1.42809 19.9037C0.63218 20.2807 -0.22718 19.5193 0.05132 18.6838L2.61258 11H8.9873C9.5396 11 9.9873 10.5523 9.9873 10C9.9873 9.4477 9.5396 9 8.9873 9H2.61258z" fill="currentColor"/>
+                                            </svg>
+                                        )}
                                         {isTimeUp || isQuizFinished ? "Quiz Over" : "Send"}
                                         {/* <a href="https://iconscout.com/icons/send" class="text-underline font-size-sm" target="_blank">Send</a> by <a href="https://iconscout.com/contributors/google-inc" class="text-underline font-size-sm">Google Inc.</a> on <a href="https://iconscout.com" class="text-underline font-size-sm">IconScout</a> */}
                                     </button>
